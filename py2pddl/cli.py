288174e8d2c99342f666c008053278d70c6cced0
raibosome <raimi.bkarim@gmail.com>
2020-11-14 14:36:51 +0800

    Move files

--- py2pddl/cli.py
+++ py2pddl/cli.py
@@ -1,4 +1,37 @@
+import importlib
+from pathlib import Path
 import fire
+
+def parse(infile: str,
+          domain="domain",
+          problem="problem"):
+    """Parse a Python module that contains both a Domain and Problem class
+    definitions
+
+    Args:
+        infile (str): Name of Python file containing both the Domain
+            and Problem class definitions.
+        domain (str, optional): Base name of domain PDDL file.
+            Defaults to "domain".
+        problem (str, optional): Base name of problem PDDL file.
+            Defaults to "problem".
+    """
+    # Import module
+    p = Path(infile)
+
+    spec = importlib.util.spec_from_file_location(p.stem, str(p.resolve()))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    #importlib.reload(module)  # there might be a better way for this
+
+    problem_name = [attr for attr in dir(module)
+                    if attr.endswith("Problem")][0]
+    Problem = getattr(module, problem_name)
+
+    p = Problem()
+    p.generate_domain_pddl(filename=domain)
+    p.generate_problem_pddl(filename=problem)
 
 
 def init(filename="domain.py"):
@@ -82,7 +115,3 @@ class {class_name}Problem({class_name}Domain):
     with open(filename, "w", encoding="utf-8") as f:
         f.write(template)
         print(f"File written to {filename}")
-
-
-if __name__ == "__main__":
-    fire.Fire(init)
--- py2pddl/py2pddl.py
+++ py2pddl/py2pddl.py
@@ -1,11 +1,6 @@
 import inspect
-import importlib
-from abc import ABCMeta
-from pathlib import Path
 from functools import wraps
 from collections import UserString, UserDict
-
-import fire
 
 #pylint:disable=invalid-name
 
@@ -318,39 +313,3 @@ def create_objs(Class,
             {str(obj): Class(str(obj)) for obj in it})
 
     return obj_dict
-
-
-def parse(infile: str,
-          domain="domain",
-          problem="problem"):
-    """Parse a Python module that contains both a Domain and Problem class
-    definitions
-
-    Args:
-        infile (str): Name of Python file containing both the Domain
-            and Problem class definitions.
-        domain (str, optional): Base name of domain PDDL file.
-            Defaults to "domain".
-        problem (str, optional): Base name of problem PDDL file.
-            Defaults to "problem".
-    """
-    # Import module
-    p = Path(infile)
-
-    spec = importlib.util.spec_from_file_location(p.stem, str(p.resolve()))
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    #importlib.reload(module)  # there might be a better way for this
-
-    problem_name = [attr for attr in dir(module)
-                    if attr.endswith("Problem")][0]
-    Problem = getattr(module, problem_name)
-
-    p = Problem()
-    p.generate_domain_pddl(filename=domain)
-    p.generate_problem_pddl(filename=problem)
-
-
-if __name__ == "__main__":
-    fire.Fire(parse)
