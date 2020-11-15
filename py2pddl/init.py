3b0c6500577a0b87e6b6b69c4fc14f84cc32326d
raibosome <raimi.bkarim@gmail.com>
2020-11-15 18:24:15 +0800

    Renames

--- py2pddl/init.py
+++ py2pddl/init.py
@@ -1,55 +1,24 @@
 import importlib
 from pathlib import Path
 import fire
-
-def parse(infile: str,
-          domain="domain",
-          problem="problem"):
-    """Parse a Python module that contains both a Domain and Problem class
-    definitions
-
-    Args:
-        infile (str): Name of Python file containing both the Domain
-            and Problem class definitions.
-        domain (str, optional): Base name of domain PDDL file.
-            Defaults to "domain".
-        problem (str, optional): Base name of problem PDDL file.
-            Defaults to "problem".
-    """
-    # Import module
-    p = Path(infile)
-
-    spec = importlib.util.spec_from_file_location(p.stem, str(p.resolve()))
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    #importlib.reload(module)  # there might be a better way for this
-
-    problem_name = [attr for attr in dir(module)
-                    if attr.endswith("Problem")][0]
-    Problem = getattr(module, problem_name)
-
-    p = Problem()
-    p.generate_domain_pddl(filename=domain)
-    p.generate_problem_pddl(filename=problem)
-
+import sys
 
 def init(filename="domain.py"):
     class_name = input("Name: ")
-    class_name = class_name.title()
+    class_name = class_name[0].upper() + class_name[1:]
 
-    user_types = input("Types (separate by space): ")
+    user_types = input("Types (separated by space): ")
     types = [typ.title() for typ in user_types.split(" ")]
 
-    user_predicates = input("Predicated (separate by space): ")
+    user_predicates = input("Predicates (separated by space): ")
     predicates = [pred.lower() for pred in user_predicates.split(" ")]
 
-    user_actions = input("Actions (separate by space): ")
+    user_actions = input("Actions (separated by space): ")
     actions = [action.lower() for action in user_actions.split(" ")]
 
     domain_header = \
 f"""\
-from py2pddl import Domain, create_type
+from py2pddl import Domain, create_type, create_objs
 from py2pddl import predicate, action, goal, init
 
 
@@ -63,14 +32,14 @@ f"""\
 
     section_predicate = "\n".join([\
 f"""\
-    @predicate
+    @predicate(...)
     def {predicate}(self):
         \"\"\"Complete the method signature\"\"\"
 """ for predicate in predicates])
 
     section_action = "\n".join([\
 f"""\
-    @action
+    @action(...)
     def {action}(self):
         \"\"\"This should be a pass\"\"\"
         precond: list = None  # to fill in
@@ -115,3 +84,7 @@ class {class_name}Problem({class_name}Domain):
     with open(filename, "w", encoding="utf-8") as f:
         f.write(template)
         print(f"File written to {filename}")
+
+
+if __name__ == "__main__":
+    fire.Fire(init)
---
+++ py2pddl/parse.py
@@ -0,0 +1,39 @@
+import importlib
+from pathlib import Path
+import fire
+import sys
+
+def parse(infile: str,
+          domain="domain",
+          problem="problem"):
+    """Parse a Python module that contains both a Domain and Problem class
+    definitions
+
+    Args:
+        infile (str): Name of Python file containing both the Domain
+            and Problem class definitions.
+        domain (str, optional): Base name of domain PDDL file.
+            Defaults to "domain".
+        problem (str, optional): Base name of problem PDDL file.
+            Defaults to "problem".
+    """
+    # Import module
+    p = Path(infile)
+
+    spec = importlib.util.spec_from_file_location(p.stem, str(p.resolve()))
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    #importlib.reload(module)  # there might be a better way for this
+
+    problem_name = [attr for attr in dir(module)
+                    if attr.endswith("Problem")][0]
+    Problem = getattr(module, problem_name)
+
+    p = Problem()
+    p.generate_domain_pddl(filename=domain)
+    p.generate_problem_pddl(filename=problem)
+
+
+if __name__ == "__main__":
+    fire.Fire(parse)
